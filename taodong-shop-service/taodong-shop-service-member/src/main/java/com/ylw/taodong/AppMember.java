84f3776c3a903f8b3632972ff5abc3a5cb7ad7bc
yanglinwei <1962264482@qq.com>
2020-02-12 09:33:52 +0800

    Apollo分布式配置中心管理application.yml(会员模块)

--- taodong-shop-service/taodong-shop-service-member/src/main/java/com/ylw/taodong/AppMember.java
+++ taodong-shop-service/taodong-shop-service-member/src/main/java/com/ylw/taodong/AppMember.java
@@ -1,5 +1,6 @@
 package com.ylw.taodong;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class AppMember {
 
     public static void main(String[] args) {
