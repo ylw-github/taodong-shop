3bc1cd6540ba8bd6b7fb67051d3c9eb048effce0
yanglinwei <1962264482@qq.com>
2020-02-12 11:00:05 +0800

    Apollo分布式配置中心管理Swagger

--- taodong-shop-basics/taodong-shop-basics-zuul/src/main/java/com/ylw/taodong/AppGateWay.java
+++ taodong-shop-basics/taodong-shop-basics-zuul/src/main/java/com/ylw/taodong/AppGateWay.java
@@ -1,5 +1,12 @@
 package com.ylw.taodong;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +24,13 @@ import java.util.List;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class AppGateWay {
+
+    // 获取ApolloConfig
+    @ApolloConfig
+    private Config appConfig;
+
 
     public static void main(String[] args) {
         SpringApplication.run(AppGateWay.class, args);
@@ -26,14 +39,49 @@ public class AppGateWay {
     // 添加文档来源
     @Component
     @Primary
-    class DocumentationConfig implements SwaggerResourcesProvider {
+    class DocumentationConfig implements SwaggerResourcesProvider, ConfigChangeListener {
         @Override
         public List<SwaggerResource> get() {
+            // 开启监听，配置文件发生改变需要更改,onChange()回调
+            appConfig.addChangeListener(DocumentationConfig.this);
+            return resources();
+        }
+
+        @Override
+        public void onChange(ConfigChangeEvent configChangeEvent) {
+            get();
+        }
+
+        /**
+         * 从阿波罗服务器中获取resources
+         *
+         * @return
+         */
+        private List<SwaggerResource> resources() {
+
             List resources = new ArrayList<>();
+            // app-itmayiedu-order
             // 网关使用服务别名获取远程服务的SwaggerApi
-            resources.add(swaggerResource("taodong-shop-service-member", "/taodong-shop-service-member/v2/api-docs", "2.0"));
-            resources.add(swaggerResource("taodong-shop-service-weixin", "/taodong-shop-service-weixin/v2/api-docs", "2.0"));
+            String swaggerDocJson = swaggerDocument();
+            JSONArray jsonArray = JSONArray.parseArray(swaggerDocJson);
+            for (Object object : jsonArray) {
+                JSONObject jsonObject = (JSONObject) object;
+                String name = jsonObject.getString("name");
+                String location = jsonObject.getString("location");
+                String version = jsonObject.getString("version");
+                resources.add(swaggerResource(name, location, version));
+            }
             return resources;
+        }
+
+        /**
+         * 获取swaggerDocument配置
+         *
+         * @return
+         */
+        private String swaggerDocument() {
+            String property = appConfig.getProperty("taodong-shop.zuul.swagger.document", "");
+            return property;
         }
 
         private SwaggerResource swaggerResource(String name, String location, String version) {
@@ -43,7 +91,6 @@ public class AppGateWay {
             swaggerResource.setSwaggerVersion(version);
             return swaggerResource;
         }
-
     }
 
 }
